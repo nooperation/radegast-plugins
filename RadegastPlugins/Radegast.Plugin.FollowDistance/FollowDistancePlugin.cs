4f3e08437df32c54dcc72ac42fffafb78f6e0c34
nooperation <madbrahmin@gmail.com>
2015-10-06 22:39:35 -0400

    Added simple plugin that changes follow distance

---
+++ RadegastPlugins/Radegast.Plugin.FollowDistance/FollowDistancePlugin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using OpenMetaverse;
+using System.Windows.Forms;
+
+namespace Radegast.Plugin.FollowdistancePlugin
+{
+    [Radegast.Plugin(Name = "Followdistance Plugin", Description = "Followdistance plugin.", Version = "1.0")]
+    public class FollowDistance : IRadegastPlugin
+    {
+        float originalFollowDistance;
+        
+        public void StartPlugin(RadegastInstance inst)
+        {
+            originalFollowDistance = inst.State.FollowDistance;
+            inst.State.FollowDistance = 10.0f;
+        }
+
+        public void StopPlugin(RadegastInstance inst)
+        {
+            inst.State.FollowDistance = originalFollowDistance;
+        }
+    }
+}
