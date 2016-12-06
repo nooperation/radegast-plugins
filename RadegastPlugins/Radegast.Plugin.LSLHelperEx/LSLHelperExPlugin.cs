78a955db14aed798ecf001cc96874304290b684c
nooperation <madbrahmin@gmail.com>
2016-12-06 00:04:27 -0500

    Simplified commands to just say and play_gesture

--- RadegastPlugins/Radegast.Plugin.LSLHelperEx/LSLHelperExPlugin.cs
+++ RadegastPlugins/Radegast.Plugin.LSLHelperEx/LSLHelperExPlugin.cs
@@ -2,171 +2,130 @@
 using System.IO;
 using OpenMetaverse;
 using OpenMetaverse.StructuredData;
-using MySql.Data.MySqlClient;
 
 namespace Radegast.Plugin.LSLHelperEx
 {
-	[Radegast.Plugin(Name = "LSLHelperEx Plugin", Description = "Extends the LSLHelper functionality.", Version = "1.0")]
-	public class LSLHelperExPlugin : IRadegastPlugin
-	{
-		private RadegastInstance instance;
-		public bool Enabled;
-		public UUID AllowedOwner;
-		private MySqlConnection db;
+    [Radegast.Plugin(Name = "LSLHelperEx Plugin", Description = "Extends the LSLHelper functionality.", Version = "1.0")]
+    public class LSLHelperExPlugin : IRadegastPlugin
+    {
+        private RadegastInstance instance;
+        public bool Enabled;
+        public UUID AllowedOwner;
 
-		const string cmdAddUser = "insert ignore into greeted (`serverId`, `key`) values (@serverId, @key)";
+        public void StartPlugin(RadegastInstance inst)
+        {
+            instance = inst;
+            instance.Client.Self.IM += Self_IM;
 
-		public void StartPlugin(RadegastInstance inst)
-		{
-			instance = inst;
-			instance.Client.Self.IM += Self_IM;
+            Output("LSLHelperEx loaded!");
+        }
 
-			try
-			{
-				db = new MySqlConnection(File.ReadAllText(@"db_Greeter.ini"));
-                db.Open();
-			}
-			catch (Exception ex)
-			{
-                db = null;
-				Output("Failed to connect to greeter database: " + ex.Message);
-				return;
-			}
+        void Self_IM(object sender, InstantMessageEventArgs e)
+        {
+            ProcessIM(e);
+        }
 
-			Output("LSLHelperEx loaded!");
-		}
+        public void StopPlugin(RadegastInstance inst)
+        {
+            inst.Client.Self.IM -= Self_IM;
+        }
 
-		void Self_IM(object sender, InstantMessageEventArgs e)
-		{
-			ProcessIM(e);
-		}
-	
-		public void StopPlugin(RadegastInstance inst)
-		{
-			inst.Client.Self.IM -= Self_IM;
+        private void Output(string message)
+        {
+            instance.TabConsole.DisplayNotificationInChat(message, ChatBufferTextStyle.StatusBlue);
+        }
 
-            if(db != null)
+        public void LoadSettings()
+        {
+            if (!instance.Client.Network.Connected)
             {
-                db.Close();
+                return;
             }
-		}
 
-		private void Output(string message)
-		{
-			instance.TabConsole.DisplayNotificationInChat(message, ChatBufferTextStyle.StatusBlue);
-		}
-
-		private bool IsGreetingRequired(int serverId, string agentKey)
-		{
             try
             {
-                MySqlCommand cmd = new MySqlCommand(cmdAddUser, db);
-                cmd.Parameters.AddWithValue("serverId", serverId);
-                cmd.Parameters.AddWithValue("key", agentKey);
+                if (!(instance.ClientSettings["LSLHelper"] is OSDMap))
+                {
+                    return;
+                }
 
-                return cmd.ExecuteNonQuery() != 0;
+                OSDMap map = (OSDMap)instance.ClientSettings["LSLHelper"];
+                Enabled = map["enabled"];
+                AllowedOwner = map["allowed_owner"];
             }
             catch (Exception ex)
             {
-                Output("Failed to check of greeting is required: " + ex.Message);
+                Output("Exception when loading settings: " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Dispatcher for incoming IM automation
+        /// </summary>
+        /// <param name="e">Incoming message</param>
+        /// <returns>If message processed correctly, should GUI processing be halted</returns>
+        public bool ProcessIM(InstantMessageEventArgs e)
+        {
+            LoadSettings();
+
+            if (!Enabled)
+            {
                 return false;
             }
-		}
 
-		public void LoadSettings()
-		{
-			if (!instance.Client.Network.Connected)
-			{
-				return;
-			}
+            switch (e.IM.Dialog)
+            {
+                case InstantMessageDialog.MessageFromObject:
+                {
+                    if (e.IM.FromAgentID != AllowedOwner)
+                    {
+                        return true;
+                    }
 
-			try
-			{
-				if (!(instance.ClientSettings["LSLHelper"] is OSDMap))
-				{
-					return;
-				}
+                    string[] args = e.IM.Message.Trim().Split('^');
+                    if (args.Length < 1)
+                    {
+                        return false;
+                    }
 
-				OSDMap map = (OSDMap)instance.ClientSettings["LSLHelper"];
-				Enabled = map["enabled"];
-				AllowedOwner = map["allowed_owner"];
-			}
-			catch(Exception ex)
-			{
-				Output("Exception when loading settings: " + ex.Message);
-			}
-		}
+                    switch (args[0].Trim())
+                    {
+                        case "say":
+                        {
+                            if (args.Length < 2)
+                            {
+                                return false;
+                            }
 
-		/// <summary>
-		/// Dispatcher for incoming IM automation
-		/// </summary>
-		/// <param name="e">Incoming message</param>
-		/// <returns>If message processed correctly, should GUI processing be halted</returns>
-		public bool ProcessIM(InstantMessageEventArgs e)
-		{
-			LoadSettings();
+                            string msg = args[1].Trim();
+                            instance.Client.Self.Chat(msg, 0, ChatType.Normal);
+                            return true;
+                        }
+                        case "play_gesture":
+                        {
+                            if (args.Length < 2)
+                            {
+                                return false;
+                            }
 
-			if (!Enabled)
-			{
-				return false;
-			}
+                            string gesture_name = args[1].Trim();
+                            UUID gesture_uuid;
+                            if(UUID.TryParse(gesture_name, out gesture_uuid))
+                            {
+                                Output("Playing gesture: " + gesture_name);
+                                instance.Client.Self.PlayGesture(gesture_uuid);
+                                return true;
+                            }
 
-			switch (e.IM.Dialog)
-			{
-				case InstantMessageDialog.MessageFromObject:
-				{
-					if (e.IM.FromAgentID != AllowedOwner)
-					{
-						return true;
-					}
+                            Output("Invalid gesture: " + gesture_name);
+                            return false;
+                        }
+                    }
+                }
+                break;
+            }
 
-					string[] args = e.IM.Message.Trim().Split('^');
-					if (args.Length < 1)
-					{ 
-						return false;
-					}
-
-					switch (args[0].Trim())
-					{
-						case "send_chat":
-						{
-							if (args.Length < 2)
-							{
-								return false;
-							}
-
-							string msg = args[1].Trim();
-							instance.Client.Self.Chat(msg, 0, ChatType.Normal);
-							return true;
-						}
-						case "send_greeting":
-						{
-							if (args.Length < 3)
-							{
-								return false;
-							}
-
-							UUID sendTo = UUID.Zero;
-							if (!UUID.TryParse(args[1].Trim(), out sendTo))
-							{
-								return false;
-							}
-
-							if (!IsGreetingRequired(1, sendTo.ToString()))
-							{
-								return false;
-							}
-
-							string msg = args[2].Trim();
-							instance.Client.Self.Chat(msg, 0, ChatType.Normal);
-							return true;
-						}
-					}
-				}
-				break;
-			}
-
-			return false;
-		}
-	}
+            return false;
+        }
+    }
 }
