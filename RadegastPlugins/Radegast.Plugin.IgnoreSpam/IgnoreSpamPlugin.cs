4b0a13497ec91fffcc042fe57c9aba123265bf34
nooperation <madbrahmin@gmail.com>
2018-02-28 20:59:35 -0500

    Add IgnoreSpam plugin

---
+++ RadegastPlugins/Radegast.Plugin.IgnoreSpam/IgnoreSpamPlugin.cs
@@ -0,0 +1,42 @@
+﻿using OpenMetaverse;
+
+namespace Radegast.Plugin.IgnoreSpamPlugin
+{
+    [Radegast.Plugin(Name = "IgnoreSpam Plugin", Description = "IgnoreSpam plugin.", Version = "1.0")]
+    public class ReconnectPlugin : IRadegastPlugin
+    {
+        public RadegastInstance Instance { get; set; }
+
+        private void Output(string message)
+        {
+            Instance.TabConsole.DisplayNotificationInChat(message, ChatBufferTextStyle.StatusBlue);
+        }
+
+        public void StartPlugin(RadegastInstance inst)
+        {
+            Instance = inst;
+            Instance.Netcom.InstantMessageReceived += Netcom_InstantMessageReceived;
+        }
+
+        private void Netcom_InstantMessageReceived(object sender, InstantMessageEventArgs e)
+        {
+            if (e.IM.Dialog == InstantMessageDialog.SessionSend)
+            {
+                if (Instance.Groups.ContainsKey(e.IM.IMSessionID) == false)
+                {
+                    Output("Group chat from secondlife:///app/agent/" + e.IM.FromAgentID + "/about : " + e.IM.Message);
+                    var tab = Instance.TabConsole.GetTab(e.IM.IMSessionID.ToString());
+                    if (tab != null)
+                    {
+                        tab.Close();
+                    }
+                }
+            }
+        }
+
+        public void StopPlugin(RadegastInstance inst)
+        {
+            Instance.Netcom.InstantMessageReceived -= Netcom_InstantMessageReceived;
+        }
+    }
+}
